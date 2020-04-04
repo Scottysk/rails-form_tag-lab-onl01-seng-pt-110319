1eecef3aaf08d4bbf6f42e35c147b31f8a09615d
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-04-04 01:13:14 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,5 +7,10 @@ class StudentsController < ApplicationController
   def show
     @student = Student.find(params[:id])
   end
+  
+  def create
+    @student = Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    redirect_to new_student_path
+  end
 
 end
